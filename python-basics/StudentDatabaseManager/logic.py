62f4e95f2e729278a8469c3c52a8dd9aa840eab0
Yashraj200512 <yashrajjadeja477@gmail.com>
2025-12-08 21:22:03 +0530

    implemented basic CRUX CLI

--- python-basics/StudentDatabaseManager/logic.py
+++ python-basics/StudentDatabaseManager/logic.py
@@ -1,22 +1,30 @@
 import json
 import os
-import uuid 
+import random
+
 
 def load_data():
     try:
-        with open("data.json",'r') as f:
-            return json.load(f)
+         with open("data.json",'r') as f:
+          return json.load(f)
 
     except FileNotFoundError:
         return []
 
-def save_data(students):
+def save_data(studentList):
         with open("data.json","w") as f:
-            json.dump(students,f,indent=4)
+            json.dump(studentList,f,indent=4)
 
 
-def add_student(students,name,email):
-     unique_id=str(uuid.uuid4())
+def generate_id(studentLIst):
+     while True:
+          new_id=str(random.randint(1000,9999))
+          if not any(s.get('id')==new_id for s in studentLIst):
+               return new_id
+
+
+def add_student(studentList,name,email):
+     unique_id=generate_id(studentList)
 
      new_student={
           "id":unique_id,
@@ -24,10 +32,27 @@ def add_student(students,name,email):
           "email":email
      }
 
-     students.append(new_student)
+     studentList.append(new_student)
      
-     save_data(students)
+     save_data(studentList)
 
-def get_students():
-     return load_data()
 
+def get_student_data(studentList,student_id):
+   
+     for s in studentList:
+          if s.get('id')==student_id:
+               return s     
+     return None
+     
+          
+def delete_student(studentList,studentID):
+     i=-1
+     for s in studentList:
+          i=i+1
+          if s.get("id")==studentID:
+              studentList.pop(i)
+              break
+
+     save_data(studentList)
+    
+  
--- python-basics/StudentDatabaseManager/main.py
+++ python-basics/StudentDatabaseManager/main.py
@@ -1,1 +1,66 @@
-[]
+import logic
+import textwrap
+
+
+def executeUserCommand(studentList,userChoice):
+    match userChoice:
+        case "1":
+            print("\nAdding student...\n")
+            studentName=input("please enter student name: ")
+            studentEmail=input("please enter student email id: ")
+
+            logic.add_student(studentList,studentName,studentEmail)
+            print("\nstudent Added")
+        case "2":
+            displayAllStudents(studentList)
+        case "3":
+            studentId=input("please enter student id: ")
+            print("\n", end="") 
+            studentData=logic.get_student_data(studentList,studentId)
+            for key,value in studentData.items():
+                 print(f"{key} :{value}")
+        case "4":
+             studentId=input("please enter student id: ")
+             print("\n", end="") 
+             logic.delete_student(studentList,studentId)
+     
+            
+
+
+def displayAllStudents(studentList):
+    
+    print("\n") 
+ 
+    for s in studentList:
+        for key,value in s.items():
+            print(f"{key} :{value}")
+        print("\n", end="") # Prints \n and stops there. No extra jump.       
+    print("__________________________________________________")
+
+
+
+
+def main():
+    studentList=logic.load_data()
+    while True:
+       
+
+       
+        print(textwrap.dedent("""
+                 1.Add student
+                 2.View All
+                 3.Search by ID
+                 4.Delete Student 
+                 5.Exit            
+               """))
+
+        userChoice=input("enter choice: ")
+
+        executeUserCommand(studentList,userChoice)
+        if userChoice=="5":
+            break
+        
+        
+
+
+main() 
