e16ee4cd33223e345b4fc3e067ef50ba14035884
Yashraj200512 <yashrajjadeja477@gmail.com>
2025-12-14 19:08:42 +0530

    commit from mint

--- Fastapi_practice/syntax&function_testingSpace/main.py
+++ Fastapi_practice/syntax&function_testingSpace/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+from enum import Enum
 app = FastAPI()
 
 
@@ -9,13 +9,28 @@ fake_db = []
 
 class StudentSchema(BaseModel):
     name : str
-    age:int
+    age:int | None=None
     email:str
+    hobbies:list[str]=[]
+
+class ModelName(str,Enum):
+    alexnet="alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
 
 @app.get("/")
 async def root():
 
     return {"message": "Hello World", "status": "active"}
+
+@app.get("/items/item")
+async def read_item():
+    return {"item_id": "item_id_string"}
+
+
+
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
@@ -28,3 +43,18 @@ async def create_student(student:StudentSchema):
         "message": "Student received successfully",
         "data": student
     }
+
+@app.get("/models/{model_name}")
+async def get_model(model_name:ModelName):
+    if model_name is ModelName.lenet:
+        return {
+            "model_name":ModelName.lenet.value, "message":"Deep learning FTW"
+        }
+    
+
+
+@app.get("/files/{file_path:path}") 
+async def read_file(file_path:str):
+    return{
+        "file_path":file_path
+    } 
