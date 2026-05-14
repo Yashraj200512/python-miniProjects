0207626765aeabd8b13fbadbb4513cdcfd55630f
Yashraj200512 <yashrajjadeja477@gmail.com>
2026-05-14 20:58:42 +0530

    added jwt

--- Fastapi_practice/MINI-PROJECTS/Secure_habit_tracker/main.py
+++ Fastapi_practice/MINI-PROJECTS/Secure_habit_tracker/main.py
@@ -1,8 +1,14 @@
+
 from fastapi import FastAPI,Query,Path
 from pydantic import BaseModel,Field
-
+import jwt
 
 app=FastAPI()
+
+SECRET_KEY="secret_dummy_password"
+ALGORITHM="HS256"
+
+
 
 fake_users_db={
     "yash":{"User":"yash", "password":"abcd123"}
@@ -42,9 +48,17 @@ async def user_login(user:UserSchema):
     if username in fake_users_db:
      
      if fake_users_db[username]["password"]==user_dict["password"]:
+        
+
+
+        token_data={"sub":username}
+
+        encoded_jwt=jwt.encode(token_data,SECRET_KEY,algorithm=ALGORITHM)
         return{
-           "status":"logged in succesfully"
-        }
+           "status":"logged in succesfully",
+           "access_token":encoded_jwt,
+           "token_type": "bearer",
+                         }
      else:
         return{
         "status":"wrong password"  
@@ -64,4 +78,49 @@ async def user_login(user:UserSchema):
         )
        return {"status": "entry created successfully"}
        
-         
+
+@app.get("/login/{username}")
+async def get_user_habits(username:str):
+   user_habits=[]
+
+   for habit in fake_habits_db:
+    if habit["owner"]==username:
+       user_habits.append(habit)
+
+
+
+   if(user_habits==[]): return{
+      "status":"failure,please enter habits first"
+
+   }
+   return{
+     "status":"succes",
+     "your habits":user_habits
+  }
+
+
+@app.post("/login/{username}")
+
+async def add_habit(username:str,habit_data:habits):
+   
+   if username in fake_users_db:
+      fake_habits_db.append({
+         "id":3,
+         "title":habit_data.title,
+         "owner":username
+      })
+    
+
+@app.delete("/login/{habit_id}")
+async def delete_habit(habit_id:int):
+   
+   for habit in fake_habits_db:
+      if habit["id"]==habit_id:
+         fake_habits_db.remove(habit)
+         return{
+            "status":"success,habit removed"
+         }
+     
+   return{
+            "status":"failure,habit not found"
+         }
--- Fastapi_practice/syntax&function_testingSpace/main.py
+++ Fastapi_practice/syntax&function_testingSpace/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Query, Path
+from typing import Annotated,Literal
+from pydantic import BaseModel,Field
 from enum import Enum
 app = FastAPI()
 
@@ -9,9 +10,11 @@ fake_db = []
 
 class StudentSchema(BaseModel):
     name : str
+    surname:str
     age:int | None=None
-    email:str
+    email:str =Field(pattern=r".+@.+\..+")
     hobbies:list[str]=[]
+    id:int 
 
 class ModelName(str,Enum):
     alexnet="alexnet"
@@ -19,6 +22,14 @@ class ModelName(str,Enum):
     lenet = "lenet"
 
 
+class FilterParams(BaseModel):
+    model_config={"extra":"forbid"}
+
+    limit:int=Field(10,gt=0,le=100)
+    skip:int=Field(10,ge=0)
+    optional: Literal["created_at","updated_at"]="created_at"
+  
+                  
 
 @app.get("/")
 async def root():
@@ -32,17 +43,42 @@ async def read_item():
 
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+@app.get("/items/{item_name}")
+async def read_item(item_id: Annotated[str | None ,Query( title="Query string",
+            description="Query string for the items to search in the database that have a good match",
+          max_length=5)],
+          item_name: Annotated[str | None , Path(min_length=3)]):
+          return {"item_id": item_id,
+                  "item_name":item_name
+                  }
 
 
 @app.post("/create-student/")
 async def create_student(student:StudentSchema):
+
+    student_dict=student.model_dump()
+    if student.name is not None:
+        fullname=student.name+student.surname
+        student_dict.update({"full_name":fullname})
     return {
         "message": "Student received successfully",
-        "data": student
+         **student_dict
     }
+
+@app.put("/create-student/{student_id}")
+async def update_student(student_id:int ,student:StudentSchema):
+    student_dict=student.model_dump()
+    
+    for key,value in student_dict.items():
+        if value is not None:
+            student_dict.update({key:value})
+    return {
+        "message": "Student updated successfully",
+         **student_dict,
+         "id":student_id
+    }
+
+
 
 @app.get("/models/{model_name}")
 async def get_model(model_name:ModelName):
@@ -50,7 +86,11 @@ async def get_model(model_name:ModelName):
         return {
             "model_name":ModelName.lenet.value, "message":"Deep learning FTW"
         }
-    
+    else:
+     return {
+        "model_name":"not matching"
+    }
+
 
 
 @app.get("/files/{file_path:path}") 
@@ -58,3 +98,9 @@ async def read_file(file_path:str):
     return{
         "file_path":file_path
     } 
+
+@app.get("/pagination/")
+async def show_page(pagination_query: Annotated[FilterParams,Query()]):
+    return{
+        "Your_page":f"pages showimg from {pagination_query.limit+pagination_query.skip} and {pagination_query.optional} just now"
+    }
